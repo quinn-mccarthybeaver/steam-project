34eb5f2309d51fe252efe8e6dc09ac5ade5480fb
triscuitcircuit <54428989+Triscuit-circuit@users.noreply.github.com>
2023-04-16 15:17:59 -0400

    more data

--- TechSpider/get_actual_gpu_pages.py
+++ TechSpider/get_actual_gpu_pages.py
@@ -12,18 +12,20 @@ import undetected_chromedriver as uc
 # direct_links_to_gpu_data.txt
 link = 'https://www.techpowerup.com'
 driver = uc.Chrome()
+driver.delete_all_cookies()
 driver.get(link)
 cookie_value = "0e5c8760ffb9213d5e8efd43ad5c15bc"
 driver.add_cookie({"name": "botcheck", "value": cookie_value})
-time.sleep(2)
+#solve captcha?
+time.sleep(40)
 
 
 
 with open('./direct_links_to_gpu_data.txt', 'r') as extensions:
-    line = 52
+    line = 77
     for a, extension in enumerate(extensions):
-        print(a)
         if a >= line:
+            print(a)
             url = link + extension[0:-1]
             driver.get(url)
 
@@ -32,12 +34,13 @@ with open('./direct_links_to_gpu_data.txt', 'r') as extensions:
 
             if header != None and header.text == "Are you Human?":
                 cookie_value = "0e5c8760ffb9213d5e8efd43ad5c15bc"
+                driver.delete_all_cookies()
                 driver.add_cookie({"name": "botcheck", "value": cookie_value})
                 print("bot check")
                 driver.get(url)
                 time.sleep(2)
 
-            if header != None and header.text == 'HTTP 429 - Too Many Requests':
+            if header != None and header.text == 'HTTP 429 - Too Many Requests' or header.text == "Are you Human?":
                 # idk something
                 print('found a 402 error')
                 break
