3fa071d06dcf1e2151d20340d014bfad6cba391c
Quinn McCarthy Beaver <quinn.mccarthybeaver@gmail.com>
2023-04-16 12:50:22 -0400

    reverted comments

--- TechSpider/rescrape.py
+++ TechSpider/rescrape.py
@@ -8,23 +8,23 @@ from os import listdir
 
 
 link = 'https://www.techpowerup.com/gpu-specs/'
-# driver = webdriver.Firefox()
+driver = webdriver.Firefox()
 
 for filename in listdir('./scraped_pages'):
     file = open('./scraped_pages/' + filename, 'r')
     local_soup = BeautifulSoup(file, 'lxml')
     header = local_soup.find('h1')
     if header != None and header.text == 'HTTP 429 - Too Many Requests':
-        # url = link + file.name[16:-5]
-        # driver.get(url)
-        # file.close()
-        # file = open('./scraped_pages/' + filename, 'w')
-        # soup = BeautifulSoup(driver.page_source, 'lxml')
-        # x = file.write(str(soup))
-        # file.close()
-        # sleep(randint(8, 12))
+        url = link + file.name[16:-5]
+        driver.get(url)
+        file.close()
+        file = open('./scraped_pages/' + filename, 'w')
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        x = file.write(str(soup))
+        file.close()
+        sleep(randint(8, 12))
         print(filename)
     else:
         file.close()
 
-# driver.close()
+driver.close()
