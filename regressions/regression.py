e016a610e119c87936644430d39eefae35c5cb66
Quinn McCarthy Beaver <quinn.mccarthybeaver@gmail.com>
2023-05-03 12:52:29 -0400

    fixed some stuff

--- regressions/regression.py
+++ regressions/regression.py
@@ -1,14 +1,9 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
-day0 = datetime.strptime('2002-01-06', '%Y-%M-%d').date()
-
-x = []
-y = []
 def load_data(filename):
     data = []
     label = []
@@ -34,28 +29,30 @@ def rmse(predict, true):
 
 def normal_rmse(predict, true):
     normalizer = np.max(true) - np.min(true)
-    result = np.sqrt(np.mean((predict - true) ** 2))
+    result = rmse(predict, true)
     result /= normalizer
-    # result = np.mean((predict - true) ** 2)
     return result
 
 model = RandomForestRegressor()
-# model = ExtraTreesRegressor()
 model.fit(x_train, y_train)
 print('random forest')
 print(rmse(model.predict(x_test), y_test))
 print(normal_rmse(model.predict(x_test), y_test))
 print(model.score(x_test, y_test))
 
+def graph_with_model(model, filename, title):
+    x_real, y_real = load_data(filename)
 
-x_real = []
-y_real = []
+    y_pred = model.predict(x_real)
 
-x_real, y_real = load_data('./graphing_data.csv')
+    plt.plot(y_pred, color='red', label='Ground Truth')
+    plt.plot(y_real, color='blue', label='Prediction')
+    plt.legend(loc='upper right')
+    plt.xlabel('Months Since Release')
+    plt.ylabel('Number of Active Users')
+    plt.title(title)
+    plt.show()
 
-y_pred = model.predict(x_real)
-
-plt.plot(y_pred, color='red', label='Ground Truth')
-plt.plot(y_real, color='blue', label='Prediction')
-plt.legend(loc='upper right')
-plt.show()
+graph_with_model(model, './Intel_HD_Graphics_4600.csv', 'Intel HD Graphics 4600')
+graph_with_model(model, './AMD_graphing_data.csv', 'AMD Radeon RX 500')
+graph_with_model(model, './Intel_graphing_data.csv', 'Intel HD Graphics 3000')
